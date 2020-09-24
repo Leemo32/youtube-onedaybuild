0348487de8207517300c480eb69256871a19eced
liam siles wright <liamsileswright@liams-air.home>
2020-09-24 21:35:38 +0930

    Added code to parse JSON response AND added Constants.swift back but removed API Key

---
+++ youtube-onedaybuild/Response.swift
@@ -0,0 +1,29 @@
+//
+//  Response.swift
+//  youtube-onedaybuild
+//
+//  Created by liam siles wright on 24/9/20.
+//
+
+import Foundation
+
+
+struct Response : Decodable {
+    
+    var items:[Video]?
+    
+    enum CodingKeys:String, CodingKey {
+        
+        case items
+        
+    }
+    
+    init (from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.items = try container.decode([Video].self, forKey: .items)
+        
+    }
+    
+}
