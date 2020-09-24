49a618d9a86e704a16284e2dc01935143bfcb598
liam siles wright <liamsileswright@liams-MacBook-Air.local>
2020-09-24 16:44:24 +0930

    Initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by liam siles wright on 24/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
