9d2a5bdf79f5ef85e68f9a8aaf9ba43096f074dc
liam siles wright <liamsileswright@liams-air.home>
2020-09-24 19:55:58 +0930

    modified gitignore to stop tracking Constants.swift

---
+++ youtube-onedaybuild/Video.swift
@@ -0,0 +1,20 @@
+//
+//  Video.swift
+//  youtube-onedaybuild
+//
+//  Created by liam siles wright on 24/9/20.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoID = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+    
+    
+}
